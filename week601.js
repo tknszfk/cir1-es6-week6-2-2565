0b0c5a955d4f42da321f0a9a1220ea32c1de93c8
tknszfk <thanakronnorrasing906@gmail.com>
2022-11-21 09:38:52 +0700

    update

--- week601.js
+++ week601.js
@@ -1,5 +1,5 @@
 let i = 1
 for (i; i < 4; i++) {
-    console.log(i)
+    console.log("2 x ",i,"=",2*i)
     
 }
---
+++ week602.js
@@ -0,0 +1,6 @@
+let i = 4
+
+for (i; i > 1; i=i-1) {
+    console.log("2 x" , i , "=" , 2*i)
+    
+}
