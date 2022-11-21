3e82ed161041553f2a4406010edc0401d693ec8c
tknszfk <thanakronnorrasing906@gmail.com>
2022-11-21 09:57:35 +0700

    update

---
+++ week603.js
@@ -0,0 +1,5 @@
+const obj = { a: 1, b: 2, c: 3 }
+
+for (let prop in obj) {
+    console.log(obj[prop])
+}
---
+++ week604.js
@@ -0,0 +1,6 @@
+let province = { p: "Trat", p: "Cgantaburi", c:2300}
+
+for (let thai in province) {
+    console.log(province[thai])
+    
+}
